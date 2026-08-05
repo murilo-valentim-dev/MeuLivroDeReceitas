8c2c9f1ff47b6ac893d4f86fef1f51f4a4248993
MURILO <murilor.valentim@outlook.com>
2026-08-04 22:01:47 -0300

    feat: adicionando endpoint for register user account

---
+++ src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    public IActionResult Register(RequestRegisterUserAccountJson request)
+    {
+        // Implement user registration logic here
+        return Created();
+    }
+}
